f4a8669d509d8ad78a4a86818907e0301f270b02
maximoyanez <maxi.yanez.2001@gmail.com>
2021-07-05 19:13:37 -0300

    version 1

---
+++ main.js
@@ -0,0 +1,34 @@
+// https://www.pexels.com/es-es/ pagina para buscar fotos de alta calidad 
+
+
+
+// Necesitamos capturar el evento del icono de menu
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    document.querySelector('.nav-menu').classList.toggle('show'); //toggle, hace aparecer a SHOW si no esta, y lo desaparece si esta
+});
+
+
+
+/* SCROLL, SE APLICA EN LAS CLASES QUE QUERRAMOS */
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+
+
+
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
+// VER PORQUE POR DEFECTO ME QUEDA EL MENU ABIIERTOO
